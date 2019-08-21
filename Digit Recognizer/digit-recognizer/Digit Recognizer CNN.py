2d605b303171d5a40c2a58ea4fdcb689eb51a2ed
ismailraju <ismailhossainraju74@gmail.com>
2019-08-22 00:37:08 +0600

    Dsffffffff'

--- Digit Recognizer/digit-recognizer/Digit Recognizer CNN.py
+++ Digit Recognizer/digit-recognizer/Digit Recognizer CNN.py
@@ -16,7 +16,8 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape = 748, activation = 'relu'))
+#classifier.add(Conv2D(32, (3, 3), input_shape = (28, 28, 1), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape = (784, 1, 1), activation = 'relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
@@ -29,7 +30,7 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 56, activation = 'relu'))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
@@ -46,18 +47,49 @@ dataset = pd.read_csv('train.csv')
 X = dataset.iloc[:, 1: ].values
 y = dataset.iloc[:, 0].values
 
+
+X_array = []
+ 
+for x in X:
+#     x[x<100]=0
+#     x[x>100]=1
+   
+     X_array.append(np.reshape(x,(-1,28)))
+    
+X=X_array
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 
-training_set = X_train
+#b=np.reshape(X_train[1],(-1,28))
+ 
 
-test_set =X_test
+#
+#training_set = X_train
+#
+#test_set =X_test
+#
+#
+#classifier.fit_generator((X_train,y_train),
+#                         steps_per_epoch = 28000,
+#                         epochs = 25,
+#                         validation_data =(X_test, y_test),
+#                         validation_steps = 14000)
 
 
-classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
-                         validation_data = test_set,
-                         validation_steps = 2000)
+from keras.preprocessing.image import ImageDataGenerator
+# initialize the number of epochs and batch size
+EPOCHS = 100
+BS = 32
+
+# construct the training image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=20, zoom_range=0.15,
+	width_shift_range=0.2, height_shift_range=0.2, shear_range=0.15,
+	horizontal_flip=True, fill_mode="nearest")
+
+# train the network
+H = classifier.fit_generator(aug.flow(X_train, y_train, batch_size=BS),
+	validation_data=(X_test, y_test), steps_per_epoch=len(X_train) // BS,
+	epochs=EPOCHS)
